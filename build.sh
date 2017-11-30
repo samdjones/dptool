9c7773584851c2ceda0fd6a09a4c5b49244e1257
Samuel Jones <sam@plan909.com>
2017-11-30 19:16:49 +0000

    working build script

--- build.sh
+++ build.sh
@@ -7,25 +7,23 @@ go clean
 rm -rf out
 # rm -rf pkg
 
-# Get our deps. Whatever version happens to be current today - yeah I know, good luck :-)
-go get github.com/inconshreveable/mousetrap
-go get github.com/spf13/cobra
-go get github.com/fsnotify/fsnotify
+# Optioally get our deps. Whatever version happens to be current today - yeah I know, good luck :-)
+# go get github.com/inconshreveable/mousetrap
+# go get github.com/spf13/cobra
+# go get github.com/fsnotify/fsnotify
 
-cd src/github.com/samdjones/dptool
-
-go build
+cd src
 
 # Mac build
-# mkdir -p out/darwin
-# GOOS=darwin GOARCH=amd64 go build -o out/darwin/dptool
+mkdir -p out/darwin
+GOOS=darwin GOARCH=amd64 go build -o ../out/darwin/dptool github.com/samdjones/dptool
 
 # Linux build
-# mkdir -p out/linux
-# GOOS=linux GOARCH=amd64 go build -o out/linux/dptool
+mkdir -p out/linux
+GOOS=linux GOARCH=amd64 go build -o ../out/linux/dptool github.com/samdjones/dptool
 
 # Windows build
-# mkdir -p out/windows
-# GOOS=windows GOARCH=amd64 go build -o out/windows/dptool.exe
+mkdir -p out/windows
+GOOS=windows GOARCH=amd64 go build -o ../out/windows/dptool.exe github.com/samdjones/dptool
 
 # Now have some fun. Or something.
