c205aa48d42a1d5ade9c4a362dc78c1f204ea379
Samuel Jones <sam@plan909.com>
2018-02-12 22:56:43 +0000

    use dep

--- src/github.com/samdjones/dptool/cmd/version.go
+++ src/github.com/samdjones/dptool/cmd/version.go
@@ -14,6 +14,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("dptool v0.1")
+		fmt.Println("dptool v0.5")
 	},
 }
